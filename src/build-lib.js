a8b542382a36fe0b4d8a84548597e20a126cd166
ryan echternacht <ryan@MacBook-Pro.local>
2020-10-08 13:50:47 -0400

    Getting eslint and a library working

---
+++ src/build-lib.js
@@ -0,0 +1,7 @@
+import DemoComponent from './components/DemoComponent'
+import CteText from './components/CteText'
+
+export {
+  CteText,
+  DemoComponent
+}
---
+++ src/components/CteText.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span
+      v-if="!editing"
+      @click="startEditing"
+    >
+      {{ value }}
+    </span>
+    <input
+      v-else
+      ref="textbox"
+      :value="value"
+      @blur="stopEditing"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CteText',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      editing: false
+    }
+  },
+  methods: {
+    startEditing () {
+      this.editing = true
+      this.$refs.textbox.focus()
+    },
+    // TODO support "save" parameter
+    stopEditing () {
+      this.editing = false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
