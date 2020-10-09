aa22bdb25781950c46afe51ea39fb6c686a3d8e4
ryan echternacht <ryan@MacBook-Pro.local>
2020-10-09 02:25:28 -0400

    Add new components to export

--- src/build-lib.js
+++ src/build-lib.js
@@ -1,7 +1,15 @@
+import BaseCte from './components/BaseCte'
 import CateDateTime from './components/CateDateTime'
+import CtePassword from './components/CtePassword'
+import CteSelect from './components/CteSelect'
 import CteText from './components/CteText'
+import CteTextarea from './components/CteTextarea'
 
 export {
+  BaseCte,
   CateDateTime,
-  CteText
+  CtePassword,
+  CteSelect,
+  CteText,
+  CteTextarea
 }
